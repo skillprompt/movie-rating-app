6d7a44139886cb8625c9b8af43c0d9df161bb656
baijanaththaru-skillprompt <baijanaththaru-skillprompt>
2025-02-02 16:38:08 +0545

    auth service

---
+++ src/mongo/auth/service.ts
@@ -0,0 +1,52 @@
+import { UserModel } from "./model";
+
+type TCreateUserInput = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+async function createUser(input: TCreateUserInput) {
+  const user = new UserModel({
+    username: input.username,
+    password: input.password,
+    email: input.email,
+  });
+  await user.save();
+}
+
+type TUpdateUserInput = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+async function updateUser(toUpdateUserId: string, input: TUpdateUserInput) {
+  const user = await UserModel.findById(toUpdateUserId);
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  await UserModel.replaceOne(
+    { _id: toUpdateUserId },
+    {
+      username: input.username,
+      email: input.email,
+      password: input.password,
+    }
+  );
+}
+
+function deleteUser() {}
+
+function getAllUsers() {}
+
+function getUserByEmail() {}
+
+function getUserById() {}
+
+export const userMongoService = {
+  createUser,
+  updateUser,
+};
